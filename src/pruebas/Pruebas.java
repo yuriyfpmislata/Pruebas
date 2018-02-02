83f6789ca4bf52ef67fa0a4fe76cadac0e904375
yuriy_clase <yuriy_clase@2A1PC20.ad.fpmislata.com>
2018-02-02 09:49:00 +0100

    fix delimitador en el StringTokenizer

--- src/pruebas/Pruebas.java
+++ src/pruebas/Pruebas.java
@@ -6,7 +6,7 @@ public class Pruebas {
 
     public static void main(String[] args) {
         String cadena = "este texto se dividirá en palabras";
-        StringTokenizer troceado = new StringTokenizer(cadena, "");
+        StringTokenizer troceado = new StringTokenizer(cadena);
         
         while (troceado.hasMoreTokens()) {
             System.out.println(troceado.nextToken());
