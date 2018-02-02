d28619c5119f91ab1560cd1b983dfa23e9e71c74
yuriy_clase <yuriy_clase@2A1PC20.ad.fpmislata.com>
2018-02-02 09:56:46 +0100

    añadida parte split

--- src/pruebas/Pruebas.java
+++ src/pruebas/Pruebas.java
@@ -5,12 +5,31 @@ import java.util.StringTokenizer;
 public class Pruebas {
 
     public static void main(String[] args) {
-        String cadena = "este texto se dividirá en palabras";
+        String cadena = "este texto se dividirá en palabras";        
+        
+        /*
+        
+        //StringTokenizer
+
         StringTokenizer troceado = new StringTokenizer(cadena);
         
         while (troceado.hasMoreTokens()) {
             System.out.println(troceado.nextToken());
         }
+        
+        */
+        
+        /* 
+        
+        //String.split()
+        
+        String[] spliteado = cadena.split("\\s");
+       
+        for (String trozo : spliteado) {
+            System.out.println(trozo);
+        }
+        
+        */
     }
 
 }
